0a72c35e5a55f6d9decf1580ecf67b91207070f5
Il Gu Yi <gubs@Il-Gu-Yiui-MacBook-Pro-Retina.local>
2015-07-17 12:01:52 +0900

    parameters

---
+++ paras.rbm.d
@@ -0,0 +1,20 @@
+// Reading Parameters setting file
+// DO NOT ERASE ALL ANNOTATION
+// Number of training data
+300
+// Number of input data dimension
+784
+// Number of hidden nodes
+10
+// Learning rate
+0.3
+// Regularization parameter
+0
+// Mini batch size
+10
+// Contrastive Divergence step (k)
+1
+// iteration max step
+10
+// Initialize type (gaussian or uniform)
+gaussian
