ead45f78f50f86c9734771991853d89506433457
Il Gu Yi <gubs@Il-Gu-Yiui-MacBook-Pro-Retina.local>
2015-08-05 00:18:17 +0900

    dataframe.h

--- dataframe.h
+++ dataframe.h
@@ -1,3 +1,11 @@
+/***********************************************************
+ * Data frame for machine learning (NN, RBM, DBN, etc)
+ *
+ * 2015. 06.
+ * modified 2015. 08. 04.
+ * by Il Gu Yi
+***********************************************************/
+
 #ifndef DATAFRAME_H
 #define DATAFRAME_H
 
@@ -35,7 +43,7 @@ class DataFrame {
 
         arma::Mat<dataType> GetData() const;
         arma::uvec GetTarget() const;
-        arma::umat GetTargetMatrix() const;
+        arma::imat GetTargetMatrix() const;
 
         dataType GetData(const unsigned& i, const unsigned& j) const;
         unsigned GetTarget(const unsigned& i) const;
@@ -43,7 +51,7 @@ class DataFrame {
 
         void SetN(const unsigned& _N);
         void SetDimension(const unsigned& _dim);
-        void SetIsTarget(bool& _isTarget);
+        void SetIsTarget(const bool& _isTarget);
         void SetDataSize(const unsigned& _N, const unsigned& _dim);
         void SetValidation(const arma::Mat<dataType>& _data, const arma::uvec& _target, const arma::imat _targetM, const arma::uvec& validindex);
 //      void SetTargetColumn(const unsigned& t);
@@ -61,7 +69,14 @@ class DataFrame {
         void CopyTarget(const arma::uvec& _target);
         void CopyTargetMatrix(const arma::imat& _targetM);
 
-        void LinearScalingEachFeatures(DataFrame<double>& _x);
+        void LinearScalingEachFeatures(DataFrame<double>& x);
+        void LinearScalingEachFeatures(DataFrame<dataType>& valid, DataFrame<dataType>& test,
+                DataFrame<double>& train_norm, DataFrame<double>& valid_norm, DataFrame<double>& test_norm);
+
+        void NormalizationEachFeatures(DataFrame<double>& x);
+        void NormalizationEachFeatures(DataFrame<dataType>& valid, DataFrame<dataType>& test,
+                DataFrame<double>& train_norm, DataFrame<double>& valid_norm, DataFrame<double>& test_norm);
+
         void TransformBinaryData();
         void SplitValidationSet(DataFrame<dataType>& valid, const unsigned& n_valid);
 
@@ -193,14 +208,7 @@ arma::Mat<dataType> DataFrame<dataType>::GetData() const { return data; }
 template<typename dataType>
 arma::uvec DataFrame<dataType>::GetTarget() const { return target; }
 template<typename dataType>
-arma::umat DataFrame<dataType>::GetTargetMatrix() const {
-    if ( !targetMatrix.size() ) {
-        cout << "Usage: Target matrix is not activated!!" << endl << "       You must SetTargetMatrix() function!!" << endl << endl;
-        exit(1);
-    }
-    else
-        return targetMatrix;
-}
+arma::imat DataFrame<dataType>::GetTargetMatrix() const { return targetMatrix; }
  
 
 template<typename dataType>
@@ -208,14 +216,7 @@ dataType DataFrame<dataType>::GetData(const unsigned& i, const unsigned& j) cons
 template<typename dataType>
 unsigned DataFrame<dataType>::GetTarget(const unsigned& i) const { return target(i); }
 template<typename dataType>
-unsigned DataFrame<dataType>::GetTargetMatrix(const unsigned& i, const unsigned& j) const {
-    if ( !targetMatrix.size() ) {
-        cout << "Usage: Target matrix is not activated!!" << endl << "       You must SetTargetMatrix() function!!" << endl << endl;
-        exit(1);
-    }
-    else
-        return targetMatrix(i, j);
-}
+unsigned DataFrame<dataType>::GetTargetMatrix(const unsigned& i, const unsigned& j) const { return targetMatrix(i, j); }
 
 
 template<typename dataType>
@@ -223,8 +224,8 @@ void DataFrame<dataType>::SetN(const unsigned& _N) { N = _N; }
 template<typename dataType>
 void DataFrame<dataType>::SetDimension(const unsigned& _dim) { dimension = _dim; }
 template<typename dataType>
-void DataFrame<dataType>::SetIsTarget(bool& _isTarget) {
-    if ( _isTarget == true ) isTarget = true;
+void DataFrame<dataType>::SetIsTarget(const bool& _isTarget) {
+    if ( _isTarget ) isTarget = true;
     else isTarget = false;
 }
 template<typename dataType>
@@ -324,13 +325,13 @@ void DataFrame<dataType>::CopyTargetMatrix(const arma::imat& _targetM) {
 
 
 template<typename dataType>
-void DataFrame<dataType>::LinearScalingEachFeatures(DataFrame<double>& _x) {
-    _x.SetN(N);
-    _x.SetDimension(dimension);
-    _x.SetIsTarget(isTarget);
-    _x.SetDataSize(N, dimension);
-    _x.CopyTarget(target);
-    _x.CopyTargetMatrix(targetMatrix);
+void DataFrame<dataType>::LinearScalingEachFeatures(DataFrame<double>& x) {
+    x.SetN(N);
+    x.SetDimension(dimension);
+    x.SetIsTarget(isTarget);
+    x.SetDataSize(N, dimension);
+    x.CopyTarget(target);
+    x.CopyTargetMatrix(targetMatrix);
 
     arma::Row<dataType> _max(dimension);
     arma::Row<dataType> _min(dimension);
@@ -341,17 +342,151 @@ void DataFrame<dataType>::LinearScalingEachFeatures(DataFrame<double>& _x) {
         if ( _max(j) != _min(j) ) {
             for (unsigned i=0; i<N; i++) {
                 double temp = ((double) data(i, j) - (double) _min(j)) / ((double) _max(j) - (double) _min(j));
-                _x.SetData(temp, i, j);
+                x.SetData(temp, i, j);
             }
         }
         else {
             for (unsigned i=0; i<N; i++) {
                 double temp = 0.0;
-                _x.SetData(temp, i, j);
+                x.SetData(temp, i, j);
             }
         }
     }
 }
+
+
+template<typename dataType>
+void DataFrame<dataType>::LinearScalingEachFeatures(DataFrame<dataType>& v, DataFrame<dataType>& t,
+            DataFrame<double>& x, DataFrame<double>& y, DataFrame<double>& z) {
+
+    x.SetN(N);                          y.SetN(v.GetN());                           z.SetN(t.GetN());
+    x.SetDimension(dimension);          y.SetDimension(dimension);                  z.SetDimension(dimension);
+    x.SetIsTarget(isTarget);            y.SetIsTarget(v.IsTarget());                z.SetIsTarget(t.IsTarget());
+    x.SetDataSize(N, dimension);        y.SetDataSize(v.GetN(), dimension);         z.SetDataSize(t.GetN(), dimension);
+    x.CopyTarget(target);               y.CopyTarget(v.GetTarget());                z.CopyTarget(t.GetTarget());
+    x.CopyTargetMatrix(targetMatrix);   y.CopyTargetMatrix(v.GetTargetMatrix());    z.CopyTargetMatrix(t.GetTargetMatrix());
+
+    arma::Row<dataType> _max(dimension), _min(dimension);
+    _max = max(data);       _min = min(data);
+
+    for (unsigned j=0; j<dimension; j++) {
+        if ( _max(j) != _min(j) ) {
+            for (unsigned i=0; i<N; i++) {
+                double temp = ((double) data(i, j) - (double) _min(j)) / ((double) _max(j) - (double) _min(j));
+                x.SetData(temp, i, j);
+            }
+
+            for (unsigned i=0; i<v.GetN(); i++) {
+                double temp = ((double) v.GetData(i, j) - (double) _min(j)) / ((double) _max(j) - (double) _min(j));
+                y.SetData(temp, i, j);
+            }
+
+            for (unsigned i=0; i<t.GetN(); i++) {
+                double temp = ((double) t.GetData(i, j) - (double) _min(j)) / ((double) _max(j) - (double) _min(j));
+                z.SetData(temp, i, j);
+            }
+        }
+        else {
+            double temp = 0.0;
+            for (unsigned i=0; i<N; i++) x.SetData(temp, i, j);
+            for (unsigned i=0; i<v.GetN(); i++) y.SetData(temp, i, j);
+            for (unsigned i=0; i<t.GetN(); i++) z.SetData(temp, i, j);
+        }
+    }
+}
+
+
+
+template<typename dataType>
+void DataFrame<dataType>::NormalizationEachFeatures(DataFrame<double>& x) {
+    x.SetN(N);
+    x.SetDimension(dimension);
+    x.SetIsTarget(isTarget);
+    x.SetDataSize(N, dimension);
+    x.CopyTarget(target);
+    x.CopyTargetMatrix(targetMatrix);
+
+    arma::vec mean(dimension), mean2(dimension);
+    mean.zeros();
+    mean2.zeros();
+    for (unsigned j=0; j<dimension; j++) {
+        for (unsigned i=0; i<N; i++) {
+            mean(j) += (double) data(i, j);
+            mean2(j) += (double) data(i, j) * (double) data(i, j);
+        }
+    }
+    mean /= (double) N;
+    mean2 /= (double) N;
+    arma::vec stdev = sqrt(mean2 - square(mean));
+
+
+    for (unsigned j=0; j<dimension; j++) {
+        if ( stdev(j) != 0.0 ) {
+            for (unsigned i=0; i<N; i++) {
+                double temp = ((double) data(i, j) - mean(j)) / stdev(j);
+                x.SetData(temp, i, j);
+            }
+        }
+        else {
+            for (unsigned i=0; i<N; i++) {
+                double temp = 0.0;
+                x.SetData(temp, i, j);
+            }
+        }
+    }
+}
+
+template<typename dataType>
+void DataFrame<dataType>::NormalizationEachFeatures(DataFrame<dataType>& v, DataFrame<dataType>& t,
+            DataFrame<double>& x, DataFrame<double>& y, DataFrame<double>& z) {
+
+    x.SetN(N);                          y.SetN(v.GetN());                           z.SetN(t.GetN());
+    x.SetDimension(dimension);          y.SetDimension(dimension);                  z.SetDimension(dimension);
+    x.SetIsTarget(isTarget);            y.SetIsTarget(v.IsTarget());                z.SetIsTarget(t.IsTarget());
+    x.SetDataSize(N, dimension);        y.SetDataSize(v.GetN(), dimension);         z.SetDataSize(t.GetN(), dimension);
+    x.CopyTarget(target);               y.CopyTarget(v.GetTarget());                z.CopyTarget(t.GetTarget());
+    x.CopyTargetMatrix(targetMatrix);   y.CopyTargetMatrix(v.GetTargetMatrix());    z.CopyTargetMatrix(t.GetTargetMatrix());
+
+    arma::vec mean(dimension), mean2(dimension);
+    mean.zeros();
+    mean2.zeros();
+    for (unsigned j=0; j<dimension; j++) {
+        for (unsigned i=0; i<N; i++) {
+            mean(j) += (double) data(i, j);
+            mean2(j) += (double) data(i, j) * (double) data(i, j);
+        }
+    }
+    mean /= (double) N;
+    mean2 /= (double) N;
+    arma::vec stdev = sqrt(mean2 - square(mean));
+
+
+    for (unsigned j=0; j<dimension; j++) {
+        if ( stdev(j) != 0.0 ) {
+            for (unsigned i=0; i<N; i++) {
+                double temp = ((double) data(i, j) - mean(j)) / stdev(j);
+                x.SetData(temp, i, j);
+            }
+
+            for (unsigned i=0; i<v.GetN(); i++) {
+                double temp = ((double) v.GetData(i, j) - mean(j)) / stdev(j);
+                y.SetData(temp, i, j);
+            }
+
+            for (unsigned i=0; i<t.GetN(); i++) {
+                double temp = ((double) t.GetData(i, j) - mean(j)) / stdev(j);
+                z.SetData(temp, i, j);
+            }
+        }
+        else {
+            double temp = 0.0;
+            for (unsigned i=0; i<N; i++) x.SetData(temp, i, j);
+            for (unsigned i=0; i<v.GetN(); i++) y.SetData(temp, i, j);
+            for (unsigned i=0; i<t.GetN(); i++) z.SetData(temp, i, j);
+        }
+    }
+}
+
 
 
 
